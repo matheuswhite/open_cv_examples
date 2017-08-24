6e079ee65f9e2e6adfa298e6fed71a4812dd1241
Matheus Tenorio dos Santos <matheustenorio@ic.ufal.br>
2017-08-24 17:03:52 -0300

    Update main
    Signed-off-by: Matheus Tenorio dos Santos <matheustenorio@ic.ufal.br>

--- main.cpp
+++ main.cpp
@@ -1,5 +1,22 @@
 #include "examples.h"
 
+Mat createWhiteDisk(const int &rows, const int &cols, const int &cx, const int &cy, const int &radius) {
+    Mat disk = Mat::zeros(rows, cols, CV_32F);
+
+    for (int x = 0; x < disk.cols; ++x) {
+        for (int y = 0; y < disk.rows; ++y) {
+            float d = pow((x-cx) * (x-cx) + (y-cy) * (y-cy), 0.5);
+            if (d <= radius)
+            {
+                disk.at<float>(y, x) = 1.0;
+                //disk.at<float>(x, y) = 1 - d / radius;
+            }
+        }
+    }
+
+    return disk;
+}
+
 Mat scaleImage2_uchar(Mat &src) {
     Mat tmp = src.clone();
     if (src.type() != CV_32F)
@@ -73,7 +90,7 @@ int example3() {
     Mat img2;
     std::vector<Mat> yrb;
     cvtColor(img, img2, CV_BGR2YCrCb);
-    split(img, yrb);
+    split(img2, yrb);
 
     imshow("img", img);
     imshow("y", yrb[0]);
@@ -86,7 +103,130 @@ int example3() {
     return 0;
 }
 
+int example4() {
+
+    namedWindow("img", WINDOW_KEEPRATIO);
+    namedWindow("h", WINDOW_KEEPRATIO);
+    namedWindow("s", WINDOW_KEEPRATIO);
+    namedWindow("v", WINDOW_KEEPRATIO);
+
+    Mat img = imread("img/baboon.png", IMREAD_COLOR);
+    Mat img2;
+    std::vector<Mat> hsv;
+    cvtColor(img, img2, CV_BGR2HSV);
+    split(img2, hsv);
+
+    imshow("img", img);
+    imshow("h", hsv[0]);
+    imshow("s", hsv[1]);
+    imshow("v", hsv[2]);
+
+    for (;;)
+        if ((char)waitKey(1) == 'q') break;
+
+    return 0;
+}
+
+int example5() {
+
+    namedWindow("img", WINDOW_KEEPRATIO);
+    namedWindow("h", WINDOW_KEEPRATIO);
+    namedWindow("s", WINDOW_KEEPRATIO);
+    namedWindow("v", WINDOW_KEEPRATIO);
+
+    Mat img = imread("img/chips.png", IMREAD_COLOR);
+    Mat img2;
+    std::vector<Mat> hsv;
+    cvtColor(img, img2, CV_BGR2HSV);
+    split(img2, hsv);
+
+    imshow("img", img);
+    imshow("h", hsv[0]);
+    imshow("s", hsv[1]);
+    imshow("v", hsv[2]);
+
+    for (;;)
+        if ((char)waitKey(1) == 'q') break;
+
+    return 0;
+}
+
+int example6() {
+
+    namedWindow("img", WINDOW_KEEPRATIO);
+    namedWindow("h", WINDOW_KEEPRATIO);
+    namedWindow("s", WINDOW_KEEPRATIO);
+    namedWindow("v", WINDOW_KEEPRATIO);
+
+    Mat img = imread("img/rgbcube_kBKG.png", IMREAD_COLOR);
+    Mat img2;
+    std::vector<Mat> hsv;
+    cvtColor(img, img2, CV_BGR2HSV);
+    split(img2, hsv);
+
+    imshow("img", img);
+    imshow("h", hsv[0]);
+    imshow("s", hsv[1]);
+    imshow("v", hsv[2]);
+
+    for (;;)
+        if ((char)waitKey(1) == 'q') break;
+
+    return 0;
+}
+
+int example7() {
+
+    namedWindow("img", WINDOW_KEEPRATIO);
+
+    int rows = 1e3;
+    int radius = (int)(rows/4);
+    int bx = (int)(rows/2), by = (int)(rows/2) - (int)(radius/2);
+    int gx =(int)(rows/2) - radius/2;
+    int gy =(int)(rows/2) + radius/2;
+    int rx =(int)(rows/2) + radius/2;
+    int ry =(int)(rows/2) + radius/2;
+    Mat img;
+    std::vector<Mat> bgr;
+    bgr.push_back(createWhiteDisk(rows, rows, bx, by, radius));
+    bgr.push_back(createWhiteDisk(rows, rows, gx, gy, radius));
+    bgr.push_back(createWhiteDisk(rows, rows, rx, ry, radius));
+    merge(bgr, img);
+    img = scaleImage2_uchar(img);
+    imshow("img", img);
+    for (;;)
+        if ((char)waitKey(1) == 'q') break;
+
+    return 0;
+}
+
+int example8() {
+
+    namedWindow("img", WINDOW_KEEPRATIO);
+
+    int rows = 1e3;
+    int radius = (int)(rows/4);
+    int bx = (int)(rows/2), by = (int)(rows/2) - (int)(radius/2);
+    int gx =(int)(rows/2) - radius/2;
+    int gy =(int)(rows/2) + radius/2;
+    int rx =(int)(rows/2) + radius/2;
+    int ry =(int)(rows/2) + radius/2;
+    Mat img;
+    std::vector<Mat> bgr;
+    bgr.push_back(createWhiteDisk(rows, rows, bx, by, radius));
+    bgr.push_back(createWhiteDisk(rows, rows, gx, gy, radius));
+    bgr.push_back(createWhiteDisk(rows, rows, rx, ry, radius));
+    merge(bgr, img);
+    img = scaleImage2_uchar(img);
+    img = Scalar(255, 255, 255) - img;
+    imshow("img", img);
+    for (;;)
+        if ((char)waitKey(1) == 'q') break;
+
+    return 0;
+}
+
 int main(int argc, char *argv[])
 {
-    return example3();
+    return example8();
 }
